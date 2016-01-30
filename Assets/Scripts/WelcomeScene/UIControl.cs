d3f85db6b2fa1598f4f40b6612935e92847e318d
Fillow <710694872@qq.com>
2016-01-30 17:50:09 +0800

    WelcomeScene的改动
    完善UI比例，点击事件
    欠缺LOGO、4个按钮图片资源

--- Assets/Scripts/WelcomeScene/UIControl.cs
+++ Assets/Scripts/WelcomeScene/UIControl.cs
@@ -1,17 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIControl : MonoBehaviour {
 
-    public GUIStyle btnStart;
-    public GUIStyle btnReset;
-    public GUIStyle btnAchievement;
-    public GUIStyle btnSettings;
+    public GUIStyle styleBtnStart;
+    public GUIStyle styleBtnReset;
+    public GUIStyle styleBtnAchievement;
+    public GUIStyle styleBtnSettings;
+
+    Vector2 sizeButton;
+    Vector2 posBtnStart, posBtnReset, posBtnAchievement, posBtnSettings;
 
 
-    void OnGUI() {
-        GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.7f, Screen.width * 0.4f, Screen.width * 0.8f), "Start", btnStart);
+    void Awake()
+    {
+        sizeButton = new Vector2(Screen.width * 0.4f, Screen.height * 0.08f);
 
+        posBtnStart = new Vector2(Screen.width * 0.05f, Screen.height * 0.73f);
+        posBtnReset = new Vector2(Screen.width * 0.55f, Screen.height * 0.73f);
+        posBtnAchievement = new Vector2(Screen.width * 0.05f, Screen.height * 0.86f);
+        posBtnSettings = new Vector2(Screen.width * 0.55f, Screen.height * 0.86f);
+    }
+
+    void OnGUI()
+    {
+        GUI.Button(new Rect(posBtnStart, sizeButton)," ", styleBtnStart);
+
+        if (GUI.Button(new Rect(posBtnReset, sizeButton), " ", styleBtnReset)) {
+            SceneManager.LoadScene("Reset", LoadSceneMode.Single);
+        }
+
+        if (GUI.Button(new Rect(posBtnAchievement, sizeButton), " ", styleBtnAchievement))
+        {
+            SceneManager.LoadScene("Achievement", LoadSceneMode.Single);
+        }
+
+        if (GUI.Button(new Rect(posBtnSettings, sizeButton), " ", styleBtnSettings))
+        {
+            SceneManager.LoadScene("Settings", LoadSceneMode.Single);
+        }
     }
 }
