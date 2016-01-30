147fd1d5f8eeff03fdf1d4bc82c615a67b952e0c
Fillow <710694872@qq.com>
2016-01-30 13:08:26 +0800

    welcome界面ui布局和还没想好的比例

---
+++ Assets/Scripts/WelcomeScene/UIControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour {
+
+    public GUIStyle btnStart;
+    public GUIStyle btnReset;
+    public GUIStyle btnAchievement;
+    public GUIStyle btnSettings;
+
+
+    void OnGUI() {
+        GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.7f, Screen.width * 0.4f, Screen.width * 0.8f), "Start", btnStart);
+
+    }
+}
