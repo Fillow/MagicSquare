d7d41437f34deabf7bdbcb5d78a52533597c55ca
Fillow <710694872@qq.com>
2016-01-30 23:59:08 +0800

    三阶魔方系统建立
    镜头位置确定，方块自身属性建立
    缺少颜色信息管理，转动方法管理

---
+++ Assets/Scripts/MagicCube/CameraFaceToCube.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFaceToCube : MonoBehaviour {
+
+	void Start () {
+        transform.LookAt(Vector3.zero);
+	}
+
+}
---
+++ Assets/Scripts/MagicCube/CubeProperties.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeProperties : MonoBehaviour {
+
+    Hashtable color;
+
+    void Awake()
+    {
+        initSelf();
+    }
+
+    public void setColor(string plane, int colorIndex)
+    {
+        color[plane] = colorIndex;
+    }
+
+    public bool checkColor(string plane, int colorIndex)
+    {
+        return (int)color[plane] == colorIndex;
+    }
+
+    void initSelf()
+    {
+        color = new Hashtable();
+        color.Add("Front", -1);
+        color.Add("Behind", -1);
+        color.Add("Left", -1);
+        color.Add("Right", -1);
+        color.Add("Up", -1);
+        color.Add("Down", -1);
+    }
+
+}
