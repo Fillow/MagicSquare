5ae22d7895f21c1317c7e9028980b965926b339a
Fillow <710694872@qq.com>
2016-02-03 19:57:59 +0800

    Rotation in Data

--- Assets/Scripts/MagicCube/CubeProperties.cs
+++ Assets/Scripts/MagicCube/CubeProperties.cs
@@ -15,12 +15,13 @@ public class CubeProperties : MonoBehaviour {
     void initSelf()
     {
         color = new Hashtable();
-        color.Add("blue", "-1");
-        color.Add("green", "-1");
-        color.Add("white", "-1");
-        color.Add("yellow", "-1");
-        color.Add("red", "-1");
-        color.Add("orange", "-1");
+        bool[] blue = new bool[3] { false, false, false };
+        bool[] green = new bool[3] { false, false, false };
+        bool[] red = new bool[3] { false, false, false };
+        bool[] orange = new bool[3] { false, false, false };
+        bool[] white = new bool[3] { false, false, false };
+        bool[] yellow = new bool[3] { false, false, false };
+
         /** Hashtable 机制
             char* colorBinaryCodePtr;   
             foreach(DictionaryEntry a in color)
@@ -33,72 +34,145 @@ public class CubeProperties : MonoBehaviour {
         /**用二进制编码表示面的朝向:
         前：001
         后：010
-        上：011
-        下：100
-        右：101
-        左：110
+        左：011
+        右：100
+        上：101
+        下：110
         */
         transformOfPresentCube = GetComponent<Transform>();
-        switch ((int)transformOfPresentCube.position.x)
+        switch ((int)transformOfPresentCube.localPosition.x)
         {
             case 1:
-                color["blue"] = "001";
-                color["green"] = "-1";
+                blue = new bool[3] { false, false, true };    //"001";
                 break;
             case 0:
-                color["blue"] = "-1";
-                color["green"] = "-1";
                 break;
             case -1:
-                color["blue"] = "-1";
-                color["green"] = "010";
+                green = new bool[3] { false, true, false }; //"010";
                 break;
         }
-        switch ((int)transformOfPresentCube.position.y)
+        switch ((int)transformOfPresentCube.localPosition.y)
         {
             case 1:
-                color["white"] = "011";
-                color["yellow"] = "-1";
+                white = new bool[3] { true, false, true }; //"101";
                 break;
             case 0:
-                color["white"] = "-1";
-                color["yellow"] = "-1";
                 break;
             case -1:
-                color["white"] = "-1";
-                color["yellow"] = "100";
+                yellow = new bool[3] { true, true, false }; //"110";
                 break;
         }
-        switch ((int)transformOfPresentCube.position.z)
+        switch ((int)transformOfPresentCube.localPosition.z)
         {
             case 1:
-                color["red"] = "101";
-                color["orange"] = "-1";
+                red = new bool[3] { true, false, false };//"100";
                 break;
             case 0:
-                color["red"] = "-1";
-                color["orange"] = "-1";
                 break;
             case -1:
-                color["red"] = "-1";
-                color["orange"] = "110";
+                orange = new bool[3] { false, true, true }; //"011";
                 break;
         }
+        color.Add("blue", blue);
+        color.Add("green", green);
+        color.Add("red", red);
+        color.Add("orange", orange);
+        color.Add("white", white);
+        color.Add("yellow", yellow);
     }
 
-    public void rotateX(bool isClockwise = true)
+    public void rotateX(bool isClockwise)
     {
-        bool[] tmp = new bool[3];
+        bool[] tmp;
+        bool[] fac1 = { false, false, true };
+        bool[] fac2 = { false, true, false };
+        Hashtable newColor = new Hashtable();
         foreach(DictionaryEntry c in color)
-        {
-            if(tmp[0] = (c.ToString()[0] == '1' ? true : false)) continue;
-            
+        {          
+            if (!(((bool[])c.Value)[0] | ((bool[])c.Value)[1] | ((bool[])c.Value)[2])) newColor.Add(c.Key, c.Value);
+            else if (((bool[])c.Value)[0] == fac1[0] && ((bool[])c.Value)[1] == fac1[1] && ((bool[])c.Value)[2] == fac1[2]) newColor.Add(c.Key, c.Value);
+            else if (((bool[])c.Value)[0] == fac2[0] && ((bool[])c.Value)[1] == fac2[1] && ((bool[])c.Value)[2] == fac2[2]) newColor.Add(c.Key, c.Value);
+            else
+            {
+                tmp = new bool[3];
+                tmp[0] = (isClockwise ^ ((bool[])c.Value)[1]) | (!(isClockwise ^ ((bool[])c.Value)[2]));
+                tmp[1] = isClockwise ^ ((bool[])c.Value)[2];
+                tmp[2] = !(isClockwise ^ ((bool[])c.Value)[1]);
+                newColor.Add(c.Key, tmp);
+            }
         }
+        color = newColor;
+    }
+
+    public void rotateY(bool isClockwise)
+    {
+        bool[] tmp;
+        bool[] fac1 = { true, false, true };
+        bool[] fac2 = { true, true, false };
+        Hashtable newColor = new Hashtable();
+        foreach (DictionaryEntry c in color)
+        {
+            if (!(((bool[])c.Value)[0] | ((bool[])c.Value)[1] | ((bool[])c.Value)[2])) newColor.Add(c.Key, c.Value);
+            else if (((bool[])c.Value)[0] == fac1[0] && ((bool[])c.Value)[1] == fac1[1] && ((bool[])c.Value)[2] == fac1[2]) newColor.Add(c.Key, c.Value);
+            else if (((bool[])c.Value)[0] == fac2[0] && ((bool[])c.Value)[1] == fac2[1] && ((bool[])c.Value)[2] == fac2[2]) newColor.Add(c.Key, c.Value);
+            else
+            {
+                tmp = new bool[3];
+                tmp[0] = (isClockwise ^ ((bool[])c.Value)[2]) & (!(isClockwise ^ ((bool[])c.Value)[1]));
+                tmp[1] = !(isClockwise ^ ((bool[])c.Value)[2]);
+                tmp[2] = isClockwise ^ ((bool[])c.Value)[1];
+                newColor.Add(c.Key, tmp);
+            }
+        }
+        color = newColor;
+    }
+
+    public void rotateZ(bool isClockwise)
+    {
+        bool[] tmp;
+        bool[] fac1 = { false, true, true };
+        bool[] fac2 = { true, false, false };
+        Hashtable newColor = new Hashtable();
+        foreach (DictionaryEntry c in color)
+        {
+            if (!(((bool[])c.Value)[0] | ((bool[])c.Value)[1] | ((bool[])c.Value)[2])) newColor.Add(c.Key, c.Value);
+            else if (((bool[])c.Value)[0] == fac1[0] && ((bool[])c.Value)[1] == fac1[1] && ((bool[])c.Value)[2] == fac1[2]) newColor.Add(c.Key, c.Value);
+            else if (((bool[])c.Value)[0] == fac2[0] && ((bool[])c.Value)[1] == fac2[1] && ((bool[])c.Value)[2] == fac2[2]) newColor.Add(c.Key, c.Value);
+            else
+            {
+                tmp = new bool[3];
+                tmp[0] = !((bool[])c.Value)[0];
+                tmp[1] = !(isClockwise ^ ((((bool[])c.Value)[0]) ^ (((bool[])c.Value)[1])));
+                tmp[2] = isClockwise ^ ((((bool[])c.Value)[0]) ^ (((bool[])c.Value)[1]));
+                newColor.Add(c.Key, tmp);
+            }
+        }
+        color = newColor;
     }
 
     public bool checkColor(string color, string plane)
     {
         return this.color[color].ToString() == plane;
+    }
+
+    public void debugLogColor()
+    {
+        string tmpStr = "";
+        foreach (DictionaryEntry c in color)
+        {
+            tmpStr = "";
+            tmpStr += ((bool[])c.Value)[0] ? "1" : "0";
+            tmpStr += ((bool[])c.Value)[1] ? "1" : "0";
+            tmpStr += ((bool[])c.Value)[2] ? "1" : "0";
+            string msg = "没有";
+            if (string.Equals(tmpStr, "001")) msg = "在 前 面";
+            else if (string.Equals(tmpStr, "010")) msg = "在 后 面";
+            else if (string.Equals(tmpStr, "011")) msg = "在 左 面";
+            else if (string.Equals(tmpStr, "100")) msg = "在 右 面";
+            else if (string.Equals(tmpStr, "101")) msg = "在 上 面";
+            else if (string.Equals(tmpStr, "110")) msg = "在 下 面";
+            Debug.Log(c.Key + msg);
+        }
     }
 
 }
--- Assets/Scripts/MagicCube/RibikCubeManager.cs
+++ Assets/Scripts/MagicCube/RibikCubeManager.cs
@@ -7,12 +7,23 @@ public class RibikCubeManager : MonoBehaviour {
 
     GameObject[] activeCubes;
 
-	void Start () {
-	
-	}
+    void Start () {
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 70, 30), "RotateX")) cubes[0].GetComponent<CubeProperties>().rotateX(true);
+        if (GUI.Button(new Rect(80, 10, 70, 30), "Rotate-X")) cubes[0].GetComponent<CubeProperties>().rotateX(false);
+        if (GUI.Button(new Rect(10, 50, 70, 30), "RotateY")) cubes[0].GetComponent<CubeProperties>().rotateY(true);
+        if (GUI.Button(new Rect(80, 50, 70, 30), "Rotate-Y")) cubes[0].GetComponent<CubeProperties>().rotateY(false);
+        if (GUI.Button(new Rect(10, 90, 70, 30), "RotateZ")) cubes[0].GetComponent<CubeProperties>().rotateZ(true);
+        if (GUI.Button(new Rect(80, 90, 70, 30), "Rotate-Z")) cubes[0].GetComponent<CubeProperties>().rotateZ(false);
+        if (GUI.Button(new Rect(10, 130, 70, 30), "LogColor")) cubes[0].GetComponent<CubeProperties>().debugLogColor();
+    }
 }
