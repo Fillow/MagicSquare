58e2a9e3ca1b6212ca78ff723343253d6f692229
Fillow <710694872@qq.com>
2016-02-05 15:50:51 +0800

    手指滑动来转动
    一个伟大的改变！

--- Assets/Scripts/MagicCube/RibikCubeManager.cs
+++ Assets/Scripts/MagicCube/RibikCubeManager.cs
@@ -6,47 +6,135 @@ using System;
 public class RibikCubeManager : MonoBehaviour, CDMessageSys {
 
     public GameObject[] cubes;
-    bool isCoolDown = true;
 
-    void Start () {
+    bool isCoolDown = true;
+    LayerMask defaultPlane, touchPlane;
+
+    GameObject clickA, clickB;
+    string plane;
+    Ray rayA, rayB;
+
+    void Start ()
+    {//Layer代号： 0 ： default   8 ： 平面信息
+        defaultPlane = 1 << 0;
+        touchPlane = 1 << 8;
     }
 	
 	void Update () {
-        
+        if (!isCoolDown) return;
+        if (Input.GetMouseButtonDown(0))//第一点，点击下去
+        {
+            rayA = Camera.main.ScreenPointToRay(Input.mousePosition);   
+            RaycastHit planeInfo, cubeInfo;
+            if (Physics.Raycast(rayA, out planeInfo, Mathf.Infinity, touchPlane.value))
+            {
+                plane = planeInfo.transform.gameObject.GetComponent<TouchPlaneProperties>().planeName;
+                Debug.Log(plane);
+            }
+            if (Physics.Raycast(rayA, out cubeInfo, Mathf.Infinity, defaultPlane.value))
+            {
+                clickA = cubeInfo.transform.parent.gameObject;
+                Debug.Log(clickA.transform.localPosition);
+            }
+        }
+        if (Input.GetMouseButton(0))//第一点，持续点击
+        {
+            if (!clickA) return;
+            rayA = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit planeInfo, hitInfo;
+
+            if (Physics.Raycast(rayA, out hitInfo, Mathf.Infinity, defaultPlane.value))
+            {
+                clickB = hitInfo.transform.parent.gameObject;
+                if (clickB != clickA)
+                {//根据平面信息来排除其中一轴，再根据方块位置信息变化排除另一轴，根据面在轴上的相对信息确定顺时针或逆时针
+                 //但是这种情况并不完全，因为如果第一次点击到角块的话，点击的点移动到其他的面上，旋转信息已表达清楚，但不能通过这种方式来检测
+                    switch (plane)
+                    {//为了之后的自由视角作准备，固定视角用这种方法很麻烦      
+                        case "Front":
+                            float frontY = clickB.transform.localPosition.y - clickA.transform.localPosition.y;
+                            float frontZ = clickB.transform.localPosition.z - clickA.transform.localPosition.z;
+                            if (Mathf.Abs(frontY) > 0.9) rotateZ((int)clickA.transform.localPosition.z, frontY > 0 ? true : false);
+                            else rotateY((int)clickA.transform.localPosition.y, frontZ > 0 ? false : true);
+                            break;
+                        case "Back":
+                            float backY = clickB.transform.localPosition.y - clickA.transform.localPosition.y;
+                            float backZ = clickB.transform.localPosition.z - clickA.transform.localPosition.z;
+                            if (Mathf.Abs(backY) > 0.9) rotateZ((int)clickA.transform.localPosition.z, backY < 0 ? true : false);
+                            else rotateY((int)clickA.transform.localPosition.y, backZ < 0 ? false : true);
+                            break;
+                        case "Right":
+                            float rightX = clickB.transform.localPosition.x - clickA.transform.localPosition.x;
+                            float rightY = clickB.transform.localPosition.y - clickA.transform.localPosition.y;
+                            if (Mathf.Abs(rightX) > 0.9) rotateY((int)clickA.transform.localPosition.y, rightX > 0 ? true : false);
+                            else rotateX((int)clickA.transform.localPosition.x, rightY > 0 ? false : true);
+                            break;
+                        case "Left":
+                            float leftX = clickB.transform.localPosition.x - clickA.transform.localPosition.x;
+                            float leftY = clickB.transform.localPosition.y - clickA.transform.localPosition.y;
+                            if (Mathf.Abs(leftX) > 0.9) rotateY((int)clickA.transform.localPosition.y, leftX < 0 ? true : false);
+                            else rotateX((int)clickA.transform.localPosition.x, leftY < 0 ? false : true);
+                            break;
+                        case "Up":
+                            float upX = clickB.transform.localPosition.x - clickA.transform.localPosition.x;
+                            float upZ = clickB.transform.localPosition.z - clickA.transform.localPosition.z;
+                            if (Mathf.Abs(upX) > 0.9) rotateZ((int)clickA.transform.localPosition.z, upX < 0 ? true : false);
+                            else rotateX((int)clickA.transform.localPosition.x, upZ < 0 ? false : true);
+                            break;
+                          case "Down":
+                            float downX = clickB.transform.localPosition.x - clickA.transform.localPosition.x;
+                            float downZ = clickB.transform.localPosition.z - clickA.transform.localPosition.z;
+                            if (Mathf.Abs(downX) > 0.9) rotateZ((int)clickA.transform.localPosition.z, downX > 0 ? true : false);
+                            else rotateX((int)clickA.transform.localPosition.x, downZ > 0 ? false : true);
+                            break;
+                    }
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            clickA = null;
+            clickB = null;
+            plane = "";
+            Debug.Log("Clear");
+        }
     }
 
     void OnGUI()
     {
         if (!isCoolDown) return;
         if(GUI.Button(new Rect(10, 10, 40, 20), "X(1)")) rotateX(1, true);
-        if (GUI.Button(new Rect(10, 30, 40, 20), "-X(1)")) rotateX(1, false);
-        if (GUI.Button(new Rect(50, 10, 40, 20), "X(0)")) rotateX(0, true);
-        if (GUI.Button(new Rect(50, 30, 40, 20), "-X(0)")) rotateX(0, false);
-        if (GUI.Button(new Rect(90, 10, 40, 20), "X(-1)")) rotateX(-1, true);
-        if (GUI.Button(new Rect(90, 30, 40, 20), "-X(-1)")) rotateX(-1, false);
-
-        if (GUI.Button(new Rect(10, 60, 40, 20), "Y(1)")) rotateY(1, true);
-        if (GUI.Button(new Rect(10, 80, 40, 20), "-Y(1)")) rotateY(1, false);
-        if (GUI.Button(new Rect(50, 60, 40, 20), "Y(0)")) rotateY(0, true);
-        if (GUI.Button(new Rect(50, 80, 40, 20), "-Y(0)")) rotateY(0, false);
-        if (GUI.Button(new Rect(90, 60, 40, 20), "Y(-1)")) rotateY(-1, true);
-        if (GUI.Button(new Rect(90, 80, 40, 20), "-Y(-1)")) rotateY(-1, false);
-
-        if (GUI.Button(new Rect(10, 110, 40, 20), "Z(1)")) rotateZ(1, true);
-        if (GUI.Button(new Rect(10, 130, 40, 20), "-Z(1)")) rotateZ(1, false);
-        if (GUI.Button(new Rect(50, 110, 40, 20), "Z(0)")) rotateZ(0, true);
-        if (GUI.Button(new Rect(50, 130, 40, 20), "-Z(0)")) rotateZ(0, false);
-        if (GUI.Button(new Rect(90, 110, 40, 20), "Z(-1)")) rotateZ(-1, true);
-        if (GUI.Button(new Rect(90, 130, 40, 20), "-Z(-1)")) rotateZ(-1, false);
     }
 
-    void rotateY(int axisY, bool isClockwise)
+    void rotateX(bool isClockwise)
     {
         int i = 0;
         isCoolDown = false;
         while (i < cubes.Length)
         {
-            if (cubes[i].transform.localPosition.y == axisY) cubes[i].GetComponent<CubeProperties>().iRotateY(isClockwise);
+            cubes[i].GetComponent<CubeProperties>().iRotateX(isClockwise);
+            i++;
+        }
+    }
+
+    void rotateY(bool isClockwise)
+    {
+        int i = 0;
+        isCoolDown = false;
+        while (i < cubes.Length)
+        {
+            cubes[i].GetComponent<CubeProperties>().iRotateY(isClockwise);
+            i++;
+        }
+    }
+
+    void rotateZ(bool isClockwise)
+    {
+        int i = 0;
+        isCoolDown = false;
+        while (i < cubes.Length)
+        {
+            cubes[i].GetComponent<CubeProperties>().iRotateZ(isClockwise);
             i++;
         }
     }
@@ -58,6 +146,17 @@ public class RibikCubeManager : MonoBehaviour, CDMessageSys {
         while (i < cubes.Length)
         {
             if (cubes[i].transform.localPosition.x == axisX) cubes[i].GetComponent<CubeProperties>().iRotateX(isClockwise);
+            i++;
+        }
+    }
+
+    void rotateY(int axisY, bool isClockwise)
+    {
+        int i = 0;
+        isCoolDown = false;
+        while (i < cubes.Length)
+        {
+            if (cubes[i].transform.localPosition.y == axisY) cubes[i].GetComponent<CubeProperties>().iRotateY(isClockwise);
             i++;
         }
     }
---
+++ Assets/Scripts/MagicCube/TouchPlaneProperties.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchPlaneProperties : MonoBehaviour {
+
+    public string planeName;
+
+}
