b35c0d1ac95aab57812d21444a5a540a6eb9e325
Fillow <710694872@qq.com>
2016-02-12 19:31:22 +0800

    魔方大改动！
    点击检测变得复杂，使得操作性大幅增加
    魔方结构整体改变，优化性能，染色系统实现变得方便
    and mooooooooooooooore……

--- Assets/Scripts/CameraFaceToCube.cs
+++ Assets/Scripts/CameraFaceToCube.cs
@@ -5,17 +5,17 @@ public class CameraFaceToCube : MonoBehaviour {
 
     public float rotSpeed = 2.0f;
 
-    float distance;
     float x;
     float detTime = 0;
 
 	void Start () {
-        distance = Vector3.Distance(Vector3.zero, transform.position);
+        Application.targetFrameRate = 60;
         transform.LookAt(Vector3.zero);
 	}
 
     void Update()
     {
+        UpdateTick();
         if(Input.GetMouseButton(0) && Input.GetMouseButton(1))
         {
             x = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
@@ -33,6 +33,61 @@ public class CameraFaceToCube : MonoBehaviour {
             }
             detTime = 0;
         }
+    }
+
+    void OnGUI()
+    {
+        DrawFps();
+    }
+
+    private void DrawFps()
+    {
+        if (mLastFps > 50)
+        {
+            GUI.color = new Color(0, 1, 0);
+        }
+        else if (mLastFps > 40)
+        {
+            GUI.color = new Color(1, 1, 0);
+        }
+        else
+        {
+            GUI.color = new Color(1.0f, 0, 0);
+        }
+
+        GUI.Label(new Rect(10, 10, Screen.width * 0.2f, Screen.height * 0.1f), "fps: " + mLastFps);
+
+    }
+
+    private long mFrameCount = 0;
+    private long mLastFrameTime = 0;
+    static long mLastFps = 0;
+    private void UpdateTick()
+    {
+        if (true)
+        {
+            mFrameCount++;
+            long nCurTime = TickToMilliSec(System.DateTime.Now.Ticks);
+            if (mLastFrameTime == 0)
+            {
+                mLastFrameTime = TickToMilliSec(System.DateTime.Now.Ticks);
+            }
+
+            if ((nCurTime - mLastFrameTime) >= 1000)
+            {
+                long fps = (long)(mFrameCount * 1.0f / ((nCurTime - mLastFrameTime) / 1000.0f));
+
+                mLastFps = fps;
+
+                mFrameCount = 0;
+
+                mLastFrameTime = nCurTime;
+            }
+        }
+    }
+    public static long TickToMilliSec(long tick)
+    {
+        return tick / (10 * 1000);
     }
 
 }
--- Assets/Scripts/MagicCube/RibikCubeManager.cs
+++ Assets/Scripts/MagicCube/RibikCubeManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
+[System.Reflection.Obfuscation(Exclude = true)]
 public class RibikCubeManager : MonoBehaviour, CDMessageSys {
 
     public GameObject[] cubes;
@@ -10,16 +11,88 @@ public class RibikCubeManager : MonoBehaviour, CDMessageSys {
     bool isCoolDown = true;
     LayerMask defaultPlane, touchPlane;
 
-    GameObject clickA, clickB;
+    GameObject targetA, targetB;
     string plane;
-    Ray rayA, rayB;
+    Ray ray;
 
     void Start ()
     {//Layer代号： 0 ： default   8 ： 平面信息
         defaultPlane = 1 << 0;
         touchPlane = 1 << 8;
+        targetA = null;
+        targetB = null;
     }
-	
+    
+    void Update()
+    {
+        if (!isCoolDown) return;
+        if (Input.touchCount>0)
+        {
+            var touchA = Input.GetTouch(0);
+            switch (touchA.phase)
+            {
+                case TouchPhase.Began:
+                    ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    RaycastHit planeInfo, cubeInfo;
+                    if (Physics.Raycast(ray, out planeInfo, Mathf.Infinity, touchPlane.value))
+                    {
+                        plane = planeInfo.transform.gameObject.GetComponent<TouchPlaneProperties>().planeName;
+                        Debug.Log(plane);
+                    }
+                    if (Physics.Raycast(ray, out cubeInfo, Mathf.Infinity, defaultPlane.value))
+                    {
+                        targetA = cubeInfo.transform.parent.gameObject;
+                        Debug.Log(targetA.transform.localPosition);
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (!targetA) return;
+                    ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    if (Physics.Raycast(ray, out planeInfo, Mathf.Infinity, touchPlane.value))
+                    {
+                        string curPlane = planeInfo.transform.gameObject.GetComponent<TouchPlaneProperties>().planeName;
+                        if (plane == curPlane)
+                        {
+                            if (Physics.Raycast(ray, out cubeInfo, Mathf.Infinity, defaultPlane.value))
+                            {
+                                targetB = cubeInfo.transform.parent.gameObject;
+                                if (targetB == targetA) return;
+                                else
+                                {
+                                    rotate(plane, targetA.transform, targetB.transform);
+                                    targetA = null;
+                                    targetB = null;
+                                    plane = "";
+                                }
+                            }
+                            else
+                            {
+                                targetA = null;
+                                targetB = null;
+                                plane = "";
+                            }
+                        }
+                        else
+                        {
+                            rotate(plane, curPlane, targetA.transform);
+                            targetA = null;
+                            targetB = null;
+                            plane = "";
+                        }
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    targetA = null;
+                    targetB = null;
+                    plane = "";
+                    break;
+            }
+        }
+    }
+
+    /** 【弃用的按键检测方法】
 	void Update () {
         if (!isCoolDown) return;
         if (Input.GetMouseButton(0) && Input.GetMouseButton(1)) return;
@@ -103,11 +176,92 @@ public class RibikCubeManager : MonoBehaviour, CDMessageSys {
             Debug.Log("Clear");
         }
     }
+    */
 
-    void OnGUI()
+    void rotate(string plane1, Transform tf1, Transform tf2)
     {
-        if (!isCoolDown) return;
-        if(GUI.Button(new Rect(10, 10, 40, 20), "X(1)")) rotateX(1, true);
+        switch (plane)
+        {//为了之后的自由视角作准备，固定视角用这种方法很麻烦      
+            case "Front":
+                float frontY = tf2.localPosition.y - tf1.localPosition.y;
+                float frontZ = tf2.localPosition.z - tf1.localPosition.z;
+                if (Mathf.Abs(frontY) > 0.9) rotateZ((int)tf1.localPosition.z, frontY > 0 ? true : false);
+                else rotateY((int)tf1.localPosition.y, frontZ > 0 ? false : true);
+                break;
+            case "Back":
+                float backY = tf2.localPosition.y - tf1.localPosition.y;
+                float backZ = tf2.localPosition.z - tf1.localPosition.z;
+                if (Mathf.Abs(backY) > 0.9) rotateZ((int)tf1.localPosition.z, backY < 0 ? true : false);
+                else rotateY((int)tf1.localPosition.y, backZ < 0 ? false : true);
+                break;
+            case "Right":
+                float rightX = tf2.localPosition.x - tf1.localPosition.x;
+                float rightY = tf2.localPosition.y - tf1.localPosition.y;
+                if (Mathf.Abs(rightX) > 0.9) rotateY((int)tf1.localPosition.y, rightX > 0 ? true : false);
+                else rotateX((int)tf1.localPosition.x, rightY > 0 ? false : true);
+                break;
+            case "Left":
+                float leftX = tf2.localPosition.x - tf1.localPosition.x;
+                float leftY = tf2.localPosition.y - tf1.localPosition.y;
+                if (Mathf.Abs(leftX) > 0.9) rotateY((int)tf1.localPosition.y, leftX < 0 ? true : false);
+                else rotateX((int)tf1.localPosition.x, leftY < 0 ? false : true);
+                break;
+            case "Up":
+                float upX = tf2.localPosition.x - tf1.localPosition.x;
+                float upZ = tf2.localPosition.z - tf1.localPosition.z;
+                if (Mathf.Abs(upX) > 0.9) rotateZ((int)tf1.localPosition.z, upX < 0 ? true : false);
+                else rotateX((int)tf1.localPosition.x, upZ < 0 ? false : true);
+                break;
+            case "Down":
+                float downX = tf2.localPosition.x - tf1.localPosition.x;
+                float downZ = tf2.localPosition.z - tf1.localPosition.z;
+                if (Mathf.Abs(downX) > 0.9) rotateZ((int)tf1.localPosition.z, downX > 0 ? true : false);
+                else rotateX((int)tf1.localPosition.x, downZ > 0 ? false : true);
+                break;
+        }
+    }
+
+    void rotate(string plane1, string plane2, Transform tf1)
+    {
+        switch (plane1+plane2)
+        {
+            case "UpRight":
+            case "RightDown":
+            case "DownLeft":
+            case "LeftUp":
+                rotateX((int) tf1.localPosition.x,true);
+                break;
+            case "RightUp":
+            case "DownRight":
+            case "LeftDown":
+            case "UpLeft":
+                rotateX((int)tf1.localPosition.x, false);
+                break;
+            case "FrontLeft":
+            case "LeftBack":
+            case "BackRight":
+            case "RightFront":
+                rotateY((int)tf1.localPosition.y, true);
+                break;
+            case "FrontRight":
+            case "RightBack":
+            case "BackLeft":
+            case "LeftFront":
+                rotateY((int)tf1.localPosition.y, false);
+                break;
+            case "FrontUp":
+            case "UpBack":
+            case "BackDown":
+            case "DownFront":
+                rotateZ((int)tf1.localPosition.z, true);
+                break;
+            case "FrontDown":
+            case "DownBack":
+            case "BackUp":
+            case "UpFront":
+                rotateZ((int)tf1.localPosition.z, false);
+                break;
+        }
     }
 
     void rotateX(bool isClockwise)
